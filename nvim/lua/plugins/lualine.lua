311a926eb8b3d002fad06cc86f8d8f1579756924
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-12-02 21:03:48 +0100

    nvim

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return{
+  'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+    require('lualine').setup({
+    option = {
+
+    theme= 'dracula'
+}
+
+
+})
+
+
+
+
+    end
+}
