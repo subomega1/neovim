4ef53df1a2a77639fbf219d4fc888bf79b7b75b3
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-12-03 20:30:32 +0100

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,7 +16,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
 require("lazy").setup("plugins")
 
 
---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return{
+  {
+    "williamboman/mason.nvim",
+    config = function()
+    require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config= function()
+    require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "clangd"}
+
+})
+    end 
+  },
+  {
+     "neovim/nvim-lspconfig",
+    config = function()
+    local lspconfig = require('lspconfig')
+    lspconfig.clangd.setup({})
+    lspconfig.lua_ls.setup({})
+
+
+    end
+
+  }
+
+
+}
